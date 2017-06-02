842cdce8cee724bc917e766c6e30d4524585edff
王大崇 <wangdachong@yunfarm.cn>
2017-06-02 10:26:14 +0800

    spring boot dependency management

--- test/src/test/java/com/wdc/learnning/TestApplicationTests.java
+++ test/src/test/java/com/wdc/learnning/TestApplicationTests.java
@@ -1,16 +1,35 @@
 package com.wdc.learnning;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Collections;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void test1() {
+        List<JSONObject> jsonObjects = Collections.singletonList(new JSONObject(Collections.singletonMap("hello", "world")));
+        jsonObjects.stream().forEach(jsonObject -> {
+            try {
+                jsonObject.put("hi", "nihao");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+
+        jsonObjects.stream().forEach(System.out::println);
+    }
 
 }
