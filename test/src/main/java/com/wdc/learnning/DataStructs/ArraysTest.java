837c902163e75bbf68102a15d8d2a81e2a771bb6
wdcwells <wdcjoy@hotmail.com>
2017-06-16 23:10:40 +0800

    add MockDataStructs.java

--- test/src/main/java/com/wdc/learnning/DataStructs/ArraysTest.java
+++ test/src/main/java/com/wdc/learnning/DataStructs/ArraysTest.java
@@ -11,12 +11,12 @@ public class ArraysTest {
     @Test
     public void testInitValues() {
         Object[] refArrayS = new Object[2];//引用数据类型
-        Arrays.stream(refArrayS).forEach(o -> System.out.println(o));
+        Arrays.stream(refArrayS).forEach(System.out::println);
         String[] strings = new String[2];//串
-        Arrays.stream(strings).forEach(s -> System.out.println(s));
+        Arrays.stream(strings).forEach(System.out::println);
         int[] ints = new int[2];//整型
-        Arrays.stream(ints).forEach(value -> System.out.println(value));
+        Arrays.stream(ints).forEach(System.out::println);
         double[] doubles = new double[2];
-        Arrays.stream(doubles).forEach(value -> System.out.println(value));
+        Arrays.stream(doubles).forEach(System.out::println);
     }
 }
