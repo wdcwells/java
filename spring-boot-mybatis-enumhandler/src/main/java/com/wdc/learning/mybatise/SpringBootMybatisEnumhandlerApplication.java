99ea0cdb33f3d9ef9eca8006ad1949e45f0facd9
王大崇 <wangdachong@yunfarm.cn>
2017-07-28 13:21:07 +0800

    漏

---
+++ spring-boot-mybatis-enumhandler/src/main/java/com/wdc/learning/mybatise/SpringBootMybatisEnumhandlerApplication.java
@@ -0,0 +1,12 @@
+package com.wdc.learning.mybatise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisEnumhandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisEnumhandlerApplication.class, args);
+	}
+}
