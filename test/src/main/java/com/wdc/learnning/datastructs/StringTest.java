6549d6e8c3d9e11c9329fd6a75a9c28b25b627ba
wdcwells <w444888@126.com>
2018-01-25 17:30:01 +0800

    算法联系，合理利用数据结构与循环条件，利用引用特性

---
+++ test/src/main/java/com/wdc/learnning/datastructs/StringTest.java
@@ -0,0 +1,8 @@
+package com.wdc.learnning.datastructs;
+
+public class StringTest {
+    public static void main(String[] args) {
+        System.out.println("abc".contains(""));
+        System.out.println('\u5d07');
+    }
+}
