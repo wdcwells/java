c6dd5f51b3086139e4728101f563eafddadddc6e
wdcwells <w444888@126.com>
2018-03-14 11:43:13 +0800

    异步测试

--- test/src/test/java/com/wdc/learnning/TestApplicationTests.java
+++ test/src/test/java/com/wdc/learnning/TestApplicationTests.java
@@ -1,18 +1,25 @@
 package com.wdc.learnning;
 
+import com.wdc.learnning.async.AsyncTask;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestApplicationTests {
+
+    @Autowired
+    private AsyncTask asyncTask;
 
     @Test
     public void contextLoads() {
@@ -31,5 +38,16 @@ public class TestApplicationTests {
 
         jsonObjects.stream().forEach(System.out::println);
     }
+
+    @Test
+    public void test2() throws ExecutionException, InterruptedException {
+        Future<String> test = asyncTask.test();
+        System.out.println("test2");
+        while (!test.isDone()) {
+            System.out.println("not done");
+        }
+        System.out.println(test.get());
+    }
+
 
 }
