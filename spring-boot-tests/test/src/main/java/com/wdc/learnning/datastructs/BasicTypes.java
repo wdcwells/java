26e96dbcbc192064838aa61e3485c5d8346aec71
王大崇 <wangdachong@weiyankeji.cn>
2018-05-23 11:44:35 +0800

    bigDecimal

--- spring-boot-tests/test/src/main/java/com/wdc/learnning/datastructs/BasicTypes.java
+++ spring-boot-tests/test/src/main/java/com/wdc/learnning/datastructs/BasicTypes.java
@@ -1,10 +1,27 @@
 package com.wdc.learnning.datastructs;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * 1*1 boolean
+ * 1*8 byte
+ * 2*8 char short
+ * 4*8 int float
+ * 8*8 long  double
+ */
 public class BasicTypes {
     public static void main(String[] args) {
-        //int char byte boolean short
-        //long float double
+//        printAZaz();
+        computeDecimal();
+    }
 
+    private static void computeDecimal() {
+        System.out.println(BigDecimal.valueOf(1).divide(BigDecimal.valueOf(3), 2, RoundingMode.HALF_UP).doubleValue());
+        System.out.println(BigDecimal.valueOf(1013.6).divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP).doubleValue());
+    }
+
+    private static void printAZaz() {
         printChar(65, 90, 10);
         printChar(97, 122, 10);
     }
