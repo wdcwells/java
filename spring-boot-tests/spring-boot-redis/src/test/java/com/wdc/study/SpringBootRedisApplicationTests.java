17728778ad38652ae4f07f85bf98598755806437
王大崇 <wangdachong@weiyankeji.cn>
2018-07-05 11:00:25 +0800

    test push

--- spring-boot-tests/spring-boot-redis/src/test/java/com/wdc/study/SpringBootRedisApplicationTests.java
+++ spring-boot-tests/spring-boot-redis/src/test/java/com/wdc/study/SpringBootRedisApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 
 @RunWith(SpringRunner.class)
@@ -16,12 +18,19 @@ public class SpringBootRedisApplicationTests {
 	private StringRedisTemplate template;
 	@Autowired
 	private CountDownLatch countDownLatch;
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
 
 	@Test
 	public void contextLoads() throws InterruptedException {
 		template.convertAndSend("chat", "Hello from Redis!");
 		countDownLatch.await();
 		System.out.println(countDownLatch.getCount());
+	}
+
+	@Test
+	public void rightPushAll() {
+		redisTemplate.opsForList().rightPushAll("wdc-list", Arrays.asList("a","b","c","d"));
 	}
 
 }
