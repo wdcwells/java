2bc51a15fbd396f9bd33f9dc8722e0b67833d3c9
王大崇 <wangdachong@weiyankeji.cn>
2018-07-12 18:58:18 +0800

    unicode 0

---
+++ spring-boot-tests/spring-boot-utils/src/main/java/com/wdc/study/test/UnicodeTest.java
@@ -0,0 +1,19 @@
+package com.wdc.study.test;
+
+/**
+ * @author wdc
+ * @date 2018/7/12
+ */
+public class UnicodeTest {
+    public static void main(String[] args) {
+        System.out.println("\\u2122=\u2122");
+//        System.out.println("\u005c"); error for input single"\"
+
+//        System.out.print("abc\nd");
+//        System.out.print("abc\rd");
+
+//        System.out.print("a\u000D \u000A");//CR LF
+//        System.out.print("b\u000D");//CR
+//        System.out.print("c\u000A");//LF
+    }
+}
