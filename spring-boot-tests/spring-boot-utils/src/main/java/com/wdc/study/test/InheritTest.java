3c1dd8d5fc57507024c3932289c58f92b96426ef
王大崇 <wangdachong@weiyankeji.cn>
2018-07-25 19:44:01 +0800

    inherit limit

---
+++ spring-boot-tests/spring-boot-utils/src/main/java/com/wdc/study/test/InheritTest.java
@@ -0,0 +1,16 @@
+package com.wdc.study.test;
+
+import com.wdc.study.test.other.Parent;
+
+/**
+ * @author wdc
+ * @date 2018/7/25
+ */
+public class InheritTest {
+
+    class Inner extends Parent {
+       void test() {
+//           System.out.println(i + s);
+       }
+    }
+}
---
+++ spring-boot-tests/spring-boot-utils/src/main/java/com/wdc/study/test/other/Parent.java
@@ -0,0 +1,10 @@
+package com.wdc.study.test.other;
+
+/**
+ * @author wdc
+ * @date 2018/7/25
+ */
+public class Parent {
+    int i = 10;
+    protected String s = "string";
+}
