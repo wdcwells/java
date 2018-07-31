44d3e22153833653a4cae02a16d42f12898917c3
王大崇 <wangdachong@weiyankeji.cn>
2018-07-31 11:22:00 +0800

    subList as a view / effects on the backing source list

--- spring-boot-tests/spring-boot-utils/src/main/java/com/wdc/study/test/CollectionTest.java
+++ spring-boot-tests/spring-boot-utils/src/main/java/com/wdc/study/test/CollectionTest.java
@@ -10,25 +10,40 @@ import java.util.List;
 public class CollectionTest {
     public static void main(String[] args) {
         toArray();
+        clear();
     }
 
     private static void toArray() {
-        class Data {
-            int id;
-
-            public Data(int id) {
-                this.id = id;
-            }
-        }
-        List<Data> source = new ArrayList<>();
-        source.add(new Data(1));
-        source.add(new Data(2));
-
+        List<Data> source = newList();
         List<Data> target = new ArrayList<>(source);//底层用toArray
         target.get(0).id = 10;
-        System.out.println(source.get(0).id);
         System.out.println(source.toArray() == target.toArray());
         System.out.println(source.toArray()[0] == target.toArray()[0]);
+    }
 
+    private static void clear() {
+        List<Data> source = newList();
+        System.out.println(source.size());
+        List<Data> subList = source.subList(source.size() / 2, source.size());//just a view
+        System.out.println(subList.size());
+        subList.clear();//also remove elements of the backing list source
+        System.out.println(source.size());
+    }
+
+
+    private static List<Data> newList() {
+        ArrayList<Data> list = new ArrayList<>();
+        list.add(new Data(1));
+        list.add(new Data(2));
+        return list;
+    }
+
+
+    static class Data {
+        int id;
+
+        public Data(int id) {
+            this.id = id;
+        }
     }
 }
