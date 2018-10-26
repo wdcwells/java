4606032d069e0b56603d1bbbe16738f92f066b69
wdcwells <wdcjoy@hotmail.com>
2018-10-26 16:15:05 +0800

    jetty test

---
+++ spring-boot-tests/spring-boot-jetty/src/main/java/com/wdc/test/JettyBoot.java
@@ -0,0 +1,11 @@
+package com.wdc.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JettyBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(JettyBoot.class, args);
+    }
+}
